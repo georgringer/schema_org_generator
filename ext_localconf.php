4bc4e09453b8d86dd9ba7fdc65e0bff716d85191
Sebastian Muszynski <s.muszynski@constructiva.de>
2019-11-15 17:15:10 +0100

    Don't override the registered transformers

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,5 @@
 // Register "schema:" namespace
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['schema'][] = 'GeorgRinger\\SchemaOrgGenerator\\ViewHelpers';
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['schema-org']['transformators'] = [
-  \GeorgRinger\SchemaOrgGenerator\Transformator\TtAddressTransformator::class,
-];
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['schema-org']['transformators'][] =
+    \GeorgRinger\SchemaOrgGenerator\Transformator\TtAddressTransformator::class;
